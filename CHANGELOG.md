98d72c6c5fe8e6bb487fa2c3602a98dfb447b740
Algae <whyis2plus2@gmail.com>
2026-09-12 19:47:36 -0600

    yup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # 0.0.2
 - Fixed an issue where the game wouldn't load the enabled patterns in a pattern pack unless the pack's menu was opened
+- Tweaked the icon's colors slightly to better reflect the SmileOS2 UI.
 
 # 0.0.1
 - Initial Release
--- src/CGPacksPlus/Plugin.cs
+++ src/CGPacksPlus/Plugin.cs
@@ -8,7 +8,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PLUGIN_FULLNAME = "whyis2plus2.ULTRAKILL.CGPacksPlus";
     public const string PLUGIN_SHORTNAME = "CGPacksPlus";
-    public const string PLUGIN_VERSION = "0.0.1";
+    public const string PLUGIN_VERSION = "0.0.2";
 
     /// <summary> We need to have an instance of this in order to do patches </summary>
     private readonly Harmony harmony = new(PLUGIN_SHORTNAME);
